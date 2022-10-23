7e9e2d0b590026e9680c6db8230d070d50ef552e
Brandon <109305092@g.nccu.edu.tw>
2022-10-23 16:34:54 +0800

    fix: cannot POST /user/profile

--- functions/routes/user.js
+++ functions/routes/user.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const router = new express.Router()
 const userProfile = require("../controller/user/profile.controller")
-// const userSign = require("../controller/user/sign.controller")
 router.get("/profile", userProfile.getProfile)
-router.post("profile", userProfile.createProfile)
+router.post("/profile", userProfile.createProfile)
 router.put("/profile", userProfile.updateProfile)
 
 module.exports = router
