27bb3f31c66aecf5a6affd1d3b6c8b99caddf86e
Brandon <109305092@g.nccu.edu.tw>
2022-10-23 22:00:55 +0800

    chore: rm old user route

--- functions/routes/product/add.js
+++ functions/routes/product/add.js
@@ -1,5 +1,5 @@
-const auth = require("../../utils/auth")
-const {admin, db} = require("../../utils/admin")
+const auth = require("../../middleware/auth")
+const { admin, db } = require("../../utils/admin")
 
 const add = async (req, res) => {
     try {
@@ -7,7 +7,7 @@ const add = async (req, res) => {
         const decodedClaims = await admin.auth().verifyIdToken(idToken)
         const uid = decodedClaims.uid
 
-        const {name, price, amount, materialList} = req.body
+        const { name, price, amount, materialList } = req.body
         const newProduct = {
             uid: uid,
             name: name,
@@ -16,9 +16,9 @@ const add = async (req, res) => {
             materialList: materialList,
         }
         await db.collection("products").add(newProduct)
-        return res.send({success: true})
+        return res.send({ success: true })
     } catch (error) {
-        return res.send({success: false, message: "unknown error"})
+        return res.send({ success: false, message: "unknown error" })
     }
 }
 
