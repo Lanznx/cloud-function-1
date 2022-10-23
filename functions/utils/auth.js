70cba1b4d116256f0f56b36797c45d070f0e384b
Brandon <109305092@g.nccu.edu.tw>
2022-10-23 22:01:14 +0800

    chore: rm old user route

--- functions/utils/auth.js
+++
@@ -1,9 +0,0 @@
-const getIdToken = (req) => {
-    const authorizationHeader = req.headers.authorization || ""
-    const components = authorizationHeader.split(" ")
-    return components.length > 1 ? components[1] : ""
-}
-
-module.exports = {
-    getIdToken,
-}
