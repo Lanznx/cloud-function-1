6fdd1ce0a3c39351efd71a26e9cbad728d3e287e
Brandon <109305092@g.nccu.edu.tw>
2022-12-03 12:31:08 +0800

    rm: unused routes

--- functions/routes/product/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = new express.Router()
-
-router.route("/add").post(require("./add"))
-router.route("/delete").post(require("./delete"))
-router.route("/update").post(require("./update"))
-
-module.exports = router
