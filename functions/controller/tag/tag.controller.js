51037c8219a04989ce12f473cfe5eb99ae8fb005
Brandon <109305092@g.nccu.edu.tw>
2022-12-15 17:51:08 +0800

    feat: DELETE tag

--- functions/controller/tag/tag.controller.js
+++ functions/controller/tag/tag.controller.js
@@ -3,6 +3,7 @@ const {
   getTagListModel,
   addTagModel,
   getTagModel,
+  deleteTagModel,
 } = require("../../model/tag/tag.model")
 
 const getAll = async (req, res) => {
@@ -70,7 +71,46 @@ const add = async (req, res) => {
   }
 }
 
+const remove = async (req, res) =>{
+  const { uid } = req.middleware
+  const { name } = req.query
+  const tagDTO = {
+    uid: uid,
+    name: name,
+  }
+  const missedKey = checkColumn(tagDTO, [])
+  if (missedKey) {
+    return res.status(400).send({
+      success: false,
+      message: `麻煩提供 ${missedKey}`,
+    })
+  }
+  const tag = await getTagModel(uid, name)
+  if (tag === -1) {
+    return res.status(400).send({
+      success: false,
+      message: "標籤不存在",
+    })
+  }
+  try {
+    const result = await deleteTagModel(tagDTO)
+    return res.status(200).send({
+      success: true,
+      message: "成功刪除標籤",
+      data: result,
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({
+      success: false,
+      message: "刪除標籤失敗，請聯絡客服",
+      err: error,
+    })
+  }
+}
+
 module.exports = {
   getAll,
   add,
+  remove,
 }
--- functions/routes/tag.js
+++ functions/routes/tag.js
@@ -4,5 +4,6 @@ const { auth } = require("../middleware/auth")
 const tag = require("../controller/tag/tag.controller.js")
 router.post("/", auth, tag.add)
 router.get("/", auth, tag.getAll)
+router.delete("/", auth, tag.remove)
 
 module.exports = router
