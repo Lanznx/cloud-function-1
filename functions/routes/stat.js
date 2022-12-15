d3b3cad4d901e437a902d9f137633f4f97ea02ce
Brandon <109305092@g.nccu.edu.tw>
2022-12-16 01:49:01 +0800

    feat: stat line chart

--- functions/routes/stat.js
+++ functions/routes/stat.js
@@ -5,5 +5,6 @@ const stat = require("../controller/stat/stat.controller.js")
 router.get("/product", auth, stat.getProductStat)
 router.get("/type", auth, stat.getTypeStat)
 router.get("/staff", auth, stat.getStaffStat)
+router.get("/line", auth, stat.getLineChart)
 
 module.exports = router
